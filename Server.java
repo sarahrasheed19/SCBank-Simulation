afab86bbb8ddd90e12459e8792d801a100d83665
Carina Caraballo <59677171+carinacarab@users.noreply.github.com>
2020-04-09 12:24:56 -0400

    server created
    Server class, starts ServerThread with created socket on port 5555. ServerThread runs while server is listening to a connection -- when it is connected to a client.

---
+++ Server.java
@@ -0,0 +1,32 @@
+/**
+ * Server class opens connection -- makes connection available to any clients. 
+ * 
+ * By Carina Caraballo
+ */
+import java.net.*;
+import java.io.*;
+
+public class Server
+{
+    public static void main(String[] args) throws IOException 
+    {
+        boolean listening = true;
+        
+        try (ServerSocket sock = new ServerSocket(5555))
+        {
+            System.out.println("Sarah & Carina Bank Incorporated Connected...");
+            
+            while(listening)
+            {
+                new ServerThread(sock.accept()).start();
+            
+            }
+            
+        } catch (IOException e) 
+        {
+            System.out.println("Port could not be accessed");
+            System.exit(-1);
+        }
+    }
+    
+}
