b42756e5d1112be556f868be1f8d5399c4bdb01d
sarahrasheed19 <sarahrasheed@mail.adelphi.edu>
2020-04-11 16:04:14 -0400

    Added Message.java for message sending

---
+++ Message.java
@@ -0,0 +1,47 @@
+
+/**
+ * Message.java will be used to send messages between server and client.
+ *
+ * Sarah Rasheed
+ * CSC 450 - Assignment 6
+*/
+
+import java.io.*;
+
+public class Message implements Serializable
+{
+    // instance variables - replace the example below with your own
+    private String sender;
+    private String message;
+
+    //constructor method for objects of class Message
+    public Message(String sender, String message){
+        this.sender = sender;
+        this.message = message;
+    }
+
+    //assigns the message to the message variable
+    public Message(String message){
+        this.message = message;
+    }
+
+    //getter for sender
+    public String getSender() {
+        return sender;
+    }
+
+    //getter for message
+    public String getMessage(){
+        return message;
+    }
+
+    //setter for message
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    //setter for sender
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+}
