7c2ef05ab589312d4c7434c5ff8f6b6deb145af1
Carina Caraballo <carinacaraballo@compsci.adelphi.edu>
2020-04-15 21:10:48 -0400

    Fixed a few things

--- ServerThread.java
+++ ServerThread.java
@@ -21,24 +21,25 @@ public class ServerThread extends Thread
     }
     
     public void run() {
-        try (
-            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+        try {
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             ObjectInputStream channelIn = new ObjectInputStream(sock.getInputStream());
             ObjectOutputStream channelOut = new ObjectOutputStream(sock.getOutputStream());
-            
-        ){
+        
             String input, output;
             Message inBank, outBank;
             String name = "S&C Bank Inc.";
             
             SCProtocol exchange = new SCProtocol();
-            
-            output = exchange.processInput("new");
+            //good up to here
+
+            output = exchange.processInput(null);
             outBank = new Message(name,output);
+            channelOut.writeObject(outBank);
             
             while((inBank = (Message)channelIn.readObject()) != null){
                 input = inBank.getMessage();
+                System.out.println(input);
                 if (input.equals("4")){
                     System.out.println("Thank you for banking with Sarah and Carina Bank Incorporated!");
                     break;
@@ -46,13 +47,16 @@ public class ServerThread extends Thread
                 for (int i= 0; i<10; i++){
                     if (input.contains(String.valueOf(i))){
                         output = exchange.processInput(Float.parseFloat(input));
+                        System.out.println(output);
                         outBank.setMessage(output);
                         channelOut.writeObject(outBank);
                         //out.println(output);
                     }
                     else {
                         output = exchange.processInput(input);
+                        System.out.println(output);
                         outBank.setMessage(output);
+                        System.out.println(outBank.getMessage());
                         channelOut.writeObject(outBank);
                         //out.println(output);
                     }
@@ -62,7 +66,7 @@ public class ServerThread extends Thread
             channelIn.close();
             channelOut.close();
             sock.close();
-
+        
         }catch (IOException e) {
         } catch (ClassNotFoundException f){
         }
